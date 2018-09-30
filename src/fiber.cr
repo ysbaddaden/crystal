a551b40b05e8d18d2877fb7d1feb35aba36543c0
Julien Portalier <julien@portalier.com>
2018-09-30 12:30:38 +0200

    Move Event to Crystal namespace
    Renames the following structs:
    - Event::Event -> Crystal::Event
    - Event::Base -> Crystal::Event::Base
    - Event::DnsBase -> Crystal::Event::DnsBase

--- src/fiber.cr
+++ src/fiber.cr
@@ -16,7 +16,7 @@ class Fiber
   @@stack_pool = [] of Void*
 
   @stack : Void*
-  @resume_event : Event::Event?
+  @resume_event : Crystal::Event?
   @stack_top = Pointer(Void).null
   protected property stack_top : Void*
   protected property stack_bottom : Void*
