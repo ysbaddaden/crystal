323797a16f8ab61100ada0da936b065d05528c30
Julien Portalier <julien@portalier.com>
2025-11-23 17:15:28 +0100

    LLVM: use macro to generate target bindings

--- src/llvm.cr
+++ src/llvm.cr
@@ -20,85 +20,30 @@ module LLVM
     {% end %}
   end
 
-  def self.init_x86 : Nil
-    return if @@initialized_x86
-    @@initialized_x86 = true
+  # The list of LLVM targets is found in llvm/Config/Target.def
+  {% for name, target in {
+                           "AArch64"     => "aarch64",
+                           "ARM"         => "arm",
+                           "AVR"         => "avr",
+                           "WebAssembly" => "webassembly",
+                           "X86"         => "x86",
+                         } %}
+    def self.init_{{target.id}} : Nil
+      return if @@initialized_{{target.id}}
+      @@initialized_{{target.id}} = true
 
-    {% if LibLLVM::BUILT_TARGETS.includes?(:x86) %}
-      LibLLVM.initialize_x86_target_info
-      LibLLVM.initialize_x86_target
-      LibLLVM.initialize_x86_target_mc
-      LibLLVM.initialize_x86_asm_printer
-      LibLLVM.initialize_x86_asm_parser
-      LibLLVM.link_in_mc_jit
-    {% else %}
-      raise "ERROR: LLVM was built without X86 target"
-    {% end %}
-  end
-
-  def self.init_aarch64 : Nil
-    return if @@initialized_aarch64
-    @@initialized_aarch64 = true
-
-    {% if LibLLVM::BUILT_TARGETS.includes?(:aarch64) %}
-      LibLLVM.initialize_aarch64_target_info
-      LibLLVM.initialize_aarch64_target
-      LibLLVM.initialize_aarch64_target_mc
-      LibLLVM.initialize_aarch64_asm_printer
-      LibLLVM.initialize_aarch64_asm_parser
-      LibLLVM.link_in_mc_jit
-    {% else %}
-      raise "ERROR: LLVM was built without AArch64 target"
-    {% end %}
-  end
-
-  def self.init_arm : Nil
-    return if @@initialized_arm
-    @@initialized_arm = true
-
-    {% if LibLLVM::BUILT_TARGETS.includes?(:arm) %}
-      LibLLVM.initialize_arm_target_info
-      LibLLVM.initialize_arm_target
-      LibLLVM.initialize_arm_target_mc
-      LibLLVM.initialize_arm_asm_printer
-      LibLLVM.initialize_arm_asm_parser
-      LibLLVM.link_in_mc_jit
-    {% else %}
-      raise "ERROR: LLVM was built without ARM target"
-    {% end %}
-  end
-
-  def self.init_avr : Nil
-    return if @@initialized_avr
-    @@initialized_avr = true
-
-    {% if LibLLVM::BUILT_TARGETS.includes?(:avr) %}
-      LibLLVM.initialize_avr_target_info
-      LibLLVM.initialize_avr_target
-      LibLLVM.initialize_avr_target_mc
-      LibLLVM.initialize_avr_asm_printer
-      LibLLVM.initialize_avr_asm_parser
-      LibLLVM.link_in_mc_jit
-    {% else %}
-      raise "ERROR: LLVM was built without AVR target"
-    {% end %}
-  end
-
-  def self.init_webassembly : Nil
-    return if @@initialized_webassembly
-    @@initialized_webassembly = true
-
-    {% if LibLLVM::BUILT_TARGETS.includes?(:webassembly) %}
-      LibLLVM.initialize_webassembly_target_info
-      LibLLVM.initialize_webassembly_target
-      LibLLVM.initialize_webassembly_target_mc
-      LibLLVM.initialize_webassembly_asm_printer
-      LibLLVM.initialize_webassembly_asm_parser
-      LibLLVM.link_in_mc_jit
-    {% else %}
-      raise "ERROR: LLVM was built without WebAssembly target"
-    {% end %}
-  end
+      \{% if LibLLVM::BUILT_TARGETS.includes?({{target.symbolize}}) %}
+        LibLLVM.initialize_{{target.id}}_target_info
+        LibLLVM.initialize_{{target.id}}_target
+        LibLLVM.initialize_{{target.id}}_target_mc
+        LibLLVM.initialize_{{target.id}}_asm_printer
+        LibLLVM.initialize_{{target.id}}_asm_parser
+        LibLLVM.link_in_mc_jit
+      \{% else %}
+        raise "ERROR: LLVM was built without {{name.id}} target"
+      \{% end %}
+    end
+  {% end %}
 
   def self.init_native_target : Nil
     {% if flag?(:i386) || flag?(:x86_64) %}
--- src/llvm/lib_llvm/target.cr
+++ src/llvm/lib_llvm/target.cr
@@ -3,31 +3,20 @@ require "./types"
 lib LibLLVM
   type TargetDataRef = Void*
 
-  fun initialize_aarch64_target_info = LLVMInitializeAArch64TargetInfo
-  fun initialize_aarch64_target = LLVMInitializeAArch64Target
-  fun initialize_aarch64_target_mc = LLVMInitializeAArch64TargetMC
-  fun initialize_aarch64_asm_printer = LLVMInitializeAArch64AsmPrinter
-  fun initialize_aarch64_asm_parser = LLVMInitializeAArch64AsmParser
-  fun initialize_arm_target_info = LLVMInitializeARMTargetInfo
-  fun initialize_arm_target = LLVMInitializeARMTarget
-  fun initialize_arm_target_mc = LLVMInitializeARMTargetMC
-  fun initialize_arm_asm_printer = LLVMInitializeARMAsmPrinter
-  fun initialize_arm_asm_parser = LLVMInitializeARMAsmParser
-  fun initialize_avr_target_info = LLVMInitializeAVRTargetInfo
-  fun initialize_avr_target = LLVMInitializeAVRTarget
-  fun initialize_avr_target_mc = LLVMInitializeAVRTargetMC
-  fun initialize_avr_asm_printer = LLVMInitializeAVRAsmPrinter
-  fun initialize_avr_asm_parser = LLVMInitializeAVRAsmParser
-  fun initialize_webassembly_target_info = LLVMInitializeWebAssemblyTargetInfo
-  fun initialize_webassembly_target = LLVMInitializeWebAssemblyTarget
-  fun initialize_webassembly_target_mc = LLVMInitializeWebAssemblyTargetMC
-  fun initialize_webassembly_asm_printer = LLVMInitializeWebAssemblyAsmPrinter
-  fun initialize_webassembly_asm_parser = LLVMInitializeWebAssemblyAsmParser
-  fun initialize_x86_target_info = LLVMInitializeX86TargetInfo
-  fun initialize_x86_target = LLVMInitializeX86Target
-  fun initialize_x86_target_mc = LLVMInitializeX86TargetMC
-  fun initialize_x86_asm_printer = LLVMInitializeX86AsmPrinter
-  fun initialize_x86_asm_parser = LLVMInitializeX86AsmParser
+  # The list of LLVM targets is found in llvm/Config/Target.def
+  {% for name, target in {
+                           "AArch64"     => "aarch64",
+                           "ARM"         => "arm",
+                           "AVR"         => "avr",
+                           "WebAssembly" => "webassembly",
+                           "X86"         => "x86",
+                         } %}
+    fun initialize_{{target.id}}_target_info = LLVMInitialize{{name.id}}TargetInfo
+    fun initialize_{{target.id}}_target = LLVMInitialize{{name.id}}Target
+    fun initialize_{{target.id}}_target_mc = LLVMInitialize{{name.id}}TargetMC
+    fun initialize_{{target.id}}_asm_printer = LLVMInitialize{{name.id}}AsmPrinter
+    fun initialize_{{target.id}}_asm_parser = LLVMInitialize{{name.id}}AsmParser
+  {% end %}
 
   fun set_module_data_layout = LLVMSetModuleDataLayout(m : ModuleRef, dl : TargetDataRef)
 
