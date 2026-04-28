ysbaddaden/crystal

commit a34be1f9825fa18e59d773613cc433eb2c0a9daa

Fix `forward_missing_to` in `HTTP::Headers` (#16637)
`forward_missing_to` in `HTTP::Headers` is wrong because we explicitly don't want to forward all methods, as discovered in #16622.
The only relevant forwarded methods seem to be `clear` and `object_id`.

In `HTTP::Headers::Key` it seems entirely unused. Since this is a private internal type, it should be fine to remove without replacement.

src/http/headers.cr
@@ -9,8 +9,6 @@ struct HTTP::Headers
 
   # :nodoc:
   record Key, name : String do
-    forward_missing_to @name
-
     def hash(hasher)
       name.each_byte do |c|
         hasher = normalize_byte(c).hash(hasher)
@@ -348,7 +346,13 @@ struct HTTP::Headers
     invalid_value_char(value).nil?
   end
 
-  forward_missing_to @hash
+  def clear
+    @hash.clear
+  end
+
+  def object_id
+    @hash.object_id
+  end
 
   private def unsafe_add(key, value : String)
     key = wrap(key)

commit ae2991475e100c721feab36534e90e610b023767

Add `HTTP::Headers#to_json` and `#to_yaml` (#16890)
This patch fixes a regression on an undocumented behaviour, introduced in #16637. It also adds serialization for `HTTP::Headers` as properly documented feature.

src/http/headers.cr
@@ -46,6 +46,14 @@ struct HTTP::Headers
 
       byte
     end
+
+    def to_json_object_key
+      name
+    end
+
+    def to_yaml(builder : YAML::Nodes::Builder)
+      name.to_yaml(builder)
+    end
   end
 
   def initialize
@@ -340,6 +348,14 @@ struct HTTP::Headers
         io << name << ": " << value << "\r\n"
       end
     end
+  end
+
+  def to_json(builder : JSON::Builder) : Nil
+    @hash.to_json(builder)
+  end
+
+  def to_yaml(builder : YAML::Nodes::Builder) : Nil
+    @hash.to_yaml(builder)
   end
 
   def valid_value?(value : String) : Bool

spec/std/http/headers_spec.cr
@@ -1,5 +1,8 @@
 require "spec"
 require "http/headers"
+require "json"
+require "yaml"
+require "../../support/wasm32"
 
 describe HTTP::Headers do
   it "is empty" do
@@ -153,6 +156,24 @@ describe HTTP::Headers do
   it "#serialize" do
     headers = HTTP::Headers{"Foo_quux" => "bar", "Baz-Quux" => ["a", "b"]}
     headers.serialize.should eq("Foo_quux: bar\r\nBaz-Quux: a\r\nBaz-Quux: b\r\n")
+  end
+
+  describe "serializes" do
+    it "#to_json" do
+      HTTP::Headers{"Foo_quux" => "bar", "Baz-Quux" => ["a", "b"]}.to_json.should eq <<-JSON
+      {"Foo_quux":"bar","Baz-Quux":["a","b"]}
+      JSON
+    end
+
+    pending_wasm32 "#to_yaml" do
+      HTTP::Headers{"Foo_quux" => "bar", "Baz-Quux" => ["a", "b"]}.to_yaml.should eq <<-YAML
+      ---
+      Foo_quux: bar
+      Baz-Quux:
+      - a
+      - b\n
+      YAML
+    end
   end
 
   describe "#merge!" do